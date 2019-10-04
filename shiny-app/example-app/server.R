2a2b6182bcb457775b22fe8e92390a4c04e7893d
Bruce Herr II <bherr@indiana.edu>
2019-10-04 12:56:29 -0400

    Updated the example app to use AWS Athena db (config via .Renviron) and create a distribution of grades.

--- shiny-app/example-app/server.R
+++ shiny-app/example-app/server.R
@@ -8,6 +8,25 @@
 #
 
 library(shiny)
+library(odbc)
+library(DBI)
+
+# Get a connection
+DBI::dbConnect(
+  odbc::odbc(), 
+  driver = "/opt/simba/athenaodbc/lib/64/libathenaodbc_sb64.so", 
+  AwsRegion = Sys.getenv("AWS_REGION"),
+  AuthenticationType = "IAM Credentials",
+  S3OutputLocation = Sys.getenv("ATHENA_RESULTS_BUCKET"),
+  schema = Sys.getenv("ATHENA_DATABASE_ID"),
+  UID = Sys.getenv("AWS_ACCESS_KEY_ID"),
+  PWD = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+  sessionToken = Sys.getenv("AWS_SESSION_TOKEN")
+) -> con
+
+df=dbGetQuery(con, "SELECT CAST(percent_grade AS real) AS grade FROM edx_grades_persistentcoursegrade")
+
+dbDisconnect(con)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -15,7 +34,7 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
+    x    <- df[, 1] 
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
--- shiny-app/example-app/ui.R
+++ shiny-app/example-app/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Grade Distribution"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
@@ -22,7 +22,7 @@ shinyUI(fluidPage(
                    "Number of bins:",
                    min = 1,
                    max = 50,
-                   value = 30)
+                   value = 10)
     ),
     
     # Show a plot of the generated distribution
