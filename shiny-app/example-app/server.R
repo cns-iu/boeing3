e85c3cd3f10077c49362f904326014096b05568e
Bruce Herr II <bherr@indiana.edu>
2019-10-08 08:27:02 -0400

    Expanded elastic beanstalk config to be a little closer to running on AWS (needs to use a custom Dockerfile still). Changed label name in the example-app.

--- shiny-app/example-app/server.R
+++ shiny-app/example-app/server.R
@@ -34,11 +34,11 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    x    <- df[, 1] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    grades    <- df[, 1]
+    bins <- seq(min(grades), max(grades), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    hist(grades, breaks = bins, col = 'darkgray', border = 'white')
     
   })
   
